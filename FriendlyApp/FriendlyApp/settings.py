4f03688f29d315e0fd4cfc94011eddc45e673775
Kacie <kaciehawkins1@gmail.com>
2021-09-26 00:58:58 -0700

    created authentication app and added serializers

--- FriendlyApp/FriendlyApp/settings.py
+++ FriendlyApp/FriendlyApp/settings.py
@@ -37,10 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
     'authentication.apps.AuthenticationConfig',
-    'user.apps.UserConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'FriendlyApp.urls'
