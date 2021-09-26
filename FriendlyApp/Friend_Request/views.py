32161214f51ca669b57fb5c9640a80ef284e9144
Kacie <kaciehawkins1@gmail.com>
2021-09-26 10:27:17 -0700

    created view methods for friend_request app

--- FriendlyApp/Friend_Request/views.py
+++ FriendlyApp/Friend_Request/views.py
@@ -1,3 +1,32 @@
+from FriendlyApp.Friend_Request.models import Friend_Request
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse 
+from .models import User
+from rest_framework import status
+
+
+@login_required 
+def send_friend_request(request, userID):
+    from_user = request.user 
+    to_user = User.objects.get(id=to_userID)
+    friend_request, created = Friend_Request.objects.get_or_create(
+        from_user=from_user, to_user=to_user)
+    if created: 
+        return HttpResponse('friend request sent')
+    else: 
+        return HttpResponse('friend request was already sent')
+
+
+@login_required
+def accept_friend_request(request, requestID):
+    friend_request = Friend_Request.objects.get(id=requestID)
+    if friend_request.to_user == request.user:
+        friend_request.to_user.friends.add(friend_request.from_user)
+        friend_request.from_user.friends.add(friend_request.from_user)
+        return HttpResponse ('friend request accecpted')
+    else:
+        return HttpResponse ('friend request not accecpted')
+
 
 # Create your views here.
