f84c21ae7f875ce383ec19c05556bbd623ee9bea
Kacie <kaciehawkins1@gmail.com>
2021-09-26 10:42:30 -0700

    added AUTH USER MODEL to settings.py

--- FriendlyApp/FriendlyApp/settings.py
+++ FriendlyApp/FriendlyApp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'Profile',
+    'Friend_Request'
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
 
 try:
     from FriendlyApp.local_settings import *
