3b2595d3e3ec08df42fc3a2032f34d53a62b0584
Kacie <kaciehawkins1@gmail.com>
2021-09-26 10:04:13 -0700

    created profile app url paths

---
+++ FriendlyApp/Profile/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from Profile import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('profile/', views.ProfileList.as_view()),
+    path('profile/<int:pk>/', views.ProfileDetail.as_view()),
+
+]
+
